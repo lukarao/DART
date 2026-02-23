07781e8404006543badc0db210b0ca415a42cd7e
Luka <62384447+fluxyn@users.noreply.github.com>
2026-02-23 18:19:08 -0500

    drone: add sensor reading

--- drone/drone.ino
+++ drone/drone.ino
@@ -1,263 +1,72 @@
-// madflight v2.2.3
+#include <Wire.h>
+#include <MPU6050_tockn.h>
+#include <Adafruit_BMP280.h>
 
-#include "madflight_config.h"
-#include <madflight.h>
-#include "wifi_rcl.h"
+// ----- CONSTANTS -----
 
-//========================================================================================================================//
-//                                               USER-SPECIFIED VARIABLES                                                 //
-//========================================================================================================================//
+const int IMU_SDA = 23;
+const int IMU_SCL = 22;
 
-//IMPORTANT: This is a safety feature which keeps props spinning when armed, and hopefully reminds the pilot to disarm!!! 
-const float armed_min_throttle = 0.20; //Minimum throttle when armed, set to a value between ~0.10 and ~0.25 which keeps the props spinning at minimum speed.
+const int BAR_SDA = 21;
+const int BAR_SCL = 19;
 
-//Controller parameters (take note of defaults before modifying!): 
-const float i_limit        = 25.0;      //Integrator saturation level, mostly for safety (default 25.0)
-const float maxRoll        = 30.0;      //Max roll angle in degrees for angle mode (maximum ~70 degrees)
-const float maxPitch       = 30.0;      //Max pitch angle in degrees for angle mode (maximum ~70 degrees)
-const float maxRollRate    = 60.0;      //Max roll rate in deg/sec for rate mode 
-const float maxPitchRate   = 60.0;      //Max pitch rate in deg/sec for rate mode
-const float maxYawRate     = 160.0;     //Max yaw rate in deg/sec for angle and rate mode
+// ----- END CONSTANTS -----
 
-//PID Angle Mode 
-const float Kp_ro_pi_angle  = 0.2;      //Roll/Pitch P-gain
-const float Ki_ro_pi_angle  = 0.1;      //Roll/Pitch I-gain
-const float Kd_ro_pi_angle  = 0.05;     //Roll/Pitch D-gain
-const float Kp_yaw_angle    = 0.6;      //Yaw P-gain
-const float Kd_yaw_angle    = 0.1;      //Yaw D-gain
+TwoWire imu_i2c = TwoWire(0);
+MPU6050 imu(imu_i2c);
+TwoWire bar_i2c = TwoWire(1);
+Adafruit_BMP280 bar(&bar_i2c);
 
-//PID Rate Mode 
-const float Kp_ro_pi_rate   = 0.15;     //Roll/Pitch rate P-gain
-const float Ki_ro_pi_rate   = 0.2;      //Roll/Pitch rate I-gain
-const float Kd_ro_pi_rate   = 0.0002;   //Roll/Pitch rate D-gain (be careful when increasing too high, motors will begin to overheat!)
-const float Kp_yaw_rate     = 0.3;       //Yaw rate P-gain
-const float Ki_yaw_rate     = 0.05;      //Yaw rate I-gain
-const float Kd_yaw_rate     = 0.00015;   //Yaw rate D-gain (be careful when increasing too high, motors will begin to overheat!)
-
-//Yaw to keep in ANGLE mode when yaw stick is centered
-float yaw_desired = 0;
-
-//========================================================================================================================//
-//                                                       SETUP()                                                          //
-//========================================================================================================================//
-
-// wifi_rcl overrides rcl
-// all instances of rcl are replaced with wifi_rcl
-WifiRcl wifi_rcl;
+float groundAltitude = 0.0;
 
 void setup() {
-  //setup madflight components: Serial.begin(115200), imu, rcin, led, etc. See src/madflight/interface.h for full interface description of each component. 
-  madflight_setup();
+  Serial.begin(115200);
 
-  // Setup wifi_rcl
-  wifi_rcl.setup();
+  // Initialize IMU
+  imu_i2c.begin(IMU_SDA, IMU_SCL);
+  imu.begin();
+  Serial.println("IMU initialized");
 
-  // STOP if imu is not installed
-  if(!imu.installed()) madflight_panic("This program needs an IMU.");
+  // Calibrate IMU
+  Serial.println("Calibrating IMU");
+  imu.calcGyroOffsets();
 
-  // Setup 4 motors for the quadcopter
-  int motor_idxs[] = {0, 1, 2, 3}; //motor indexes
-  int motor_pins[] = {cfg.pin_out0, cfg.pin_out1, cfg.pin_out2, cfg.pin_out3}; //motor pins
+  // Initialize barometer
+  bar_i2c.begin(BAR_SDA, BAR_SCL);
+  if (bar.begin(0x76)) {
+    Serial.println("Barometer initialized");
+  } else {
+    Serial.println("Failed to initialize barometer");
+  }
 
-  bool success = out.setupMotors(4, motor_idxs, motor_pins, 400, 950, 2000);   // Standard PWM: 400Hz, 950-2000 us
-  if(!success) madflight_panic("Motor init failed.");
-
-  //set initial desired yaw
-  yaw_desired = ahr.yaw;
+  // Calibrate barometer
+  Serial.println("Calibrating barometer");
+  groundAltitude = bar.readAltitude();
 }
-
-//========================================================================================================================//
-//                                                            LOOP()                                                      //
-//========================================================================================================================//
 
 void loop() {
-  alt.updateAccelUp(ahr.getAccelUp(), ahr.ts); //NOTE: do this here and not in imu_loop() because `alt` object is not thread safe. - Update altitude estimator with current earth-frame up acceleration measurement
-  
-  if(bar.update()) {
-    alt.updateBarAlt(bar.alt, bar.ts); //update altitude estimator with current altitude measurement
-  }
+  imu.update();
 
-  // send current altitude measurement to hub
-  wifi_rcl.sendAlt(alt.getH());
+  Serial.print("Accel: ");
+  Serial.print(imu.getAccX());
+  Serial.print("\t");
+  Serial.print(imu.getAccY());
+  Serial.print("\t");
+  Serial.print(imu.getAccZ());
 
-  cli.update(); //process CLI commands
-}
+  Serial.print("\t");
 
-//========================================================================================================================//
-//                                                   IMU UPDATE LOOP                                                      //
-//========================================================================================================================//
+  Serial.print("Gyro: ");
+  Serial.print(imu.getGyroX());
+  Serial.print("\t");
+  Serial.print(imu.getGyroY());
+  Serial.print("\t");
+  Serial.print(imu.getGyroZ());
 
-//This is __MAIN__ function of this program. It is called when new IMU data is available.
-void imu_loop() {
-  //Sensor fusion: update ahr.roll, ahr.pitch, and ahr.yaw angle estimates (degrees) from IMU data
-  ahr.update(); 
+  Serial.print("\t");
 
-  //Get radio commands - Note: don't do this in loop() because loop() is a lower priority task than imu_loop(), so in worst case loop() will not get any processor time.
-  wifi_rcl.update();
+  Serial.print(" Altitude: ");
+  Serial.print(bar.readAltitude() - groundAltitude);
 
-  //PID Controller
-  control_Angle(wifi_rcl.throttle == 0); //Stabilize on pitch/roll angle setpoint, stabilize yaw on rate setpoint
-  
-  //Updates out.arm, the output armed flag
-  out_KillSwitchAndFailsafe(); //Cut all motor outputs if DISARMED or failsafe triggered.
-
-  //Actuator mixing
-  out_Mixer(); //Mixes PID outputs and sends command pulses to the motors, if mot.arm == true
-}
-
-//========================================================================================================================
-//                      IMU UPDATE LOOP FUNCTIONS - in same order as they are called from imu_loop()
-//========================================================================================================================
-
-//returns angle in range -180 to 180
-float degreeModulus(float v) {
-  if(v >= 180) {
-    return fmod(v + 180, 360) - 180;
-  }else if(v < -180.0) {
-    return fmod(v - 180, 360) + 180;
-  }
-  return v;
-}
-
-void control_Angle(bool zero_integrators) {
-  //DESCRIPTION: Computes control commands based on angle error
-  /*
-   * Basic PID control to stablize on angle setpoint based on desired states roll_des, pitch_des, and yaw_des. Error
-   * is simply the desired state minus the actual state (ex. roll_des - ahr.roll). Two safety features
-   * are implimented here regarding the I terms. The I terms are saturated within specified limits on startup to prevent 
-   * excessive buildup. This can be seen by holding the vehicle at an angle and seeing the motors ramp up on one side until
-   * they've maxed out throttle... saturating I to a specified limit fixes this. The second feature defaults the I terms to 0
-   * if the throttle is at the minimum setting. This means the motors will not start spooling up on the ground, and the I 
-   * terms will always start from 0 on takeoff. This function updates the variables pid.roll, pid.pitch, and pid.yaw which
-   * can be thought of as 1-D stablized signals. They are mixed to the configuration of the vehicle in out_Mixer().
-   */ 
-
-  //inputs: roll_des, pitch_des, yawRate_des
-  //outputs: pid.roll, pid.pitch, pid.yaw
-
-  //desired values
-  float roll_des = wifi_rcl.roll * maxRoll; //Between -maxRoll and +maxRoll
-  float pitch_des = wifi_rcl.pitch * maxPitch; //Between -maxPitch and +maxPitch
-  float yawRate_des = wifi_rcl.yaw * maxYawRate; //Between -maxYawRate roll_PIDand +maxYawRate
-
-  //state vars
-  static float integral_roll, integral_pitch, error_yawRate_prev, integral_yawRate;
-
-  //Zero the integrators (used to don't let integrator build if throttle is too low, or to re-start the controller)
-  if(zero_integrators) {
-    integral_roll = 0;
-    integral_pitch = 0;
-    integral_yawRate = 0;
-  }
-
-  //Roll PID
-  float error_roll = roll_des - ahr.roll;
-  integral_roll += error_roll * imu.dt;
-  integral_roll = constrain(integral_roll, -i_limit, i_limit); //Saturate integrator to prevent unsafe buildup
-  float derivative_roll = ahr.gx;
-  pid.roll = 0.01 * (Kp_ro_pi_angle*error_roll + Ki_ro_pi_angle*integral_roll - Kd_ro_pi_angle*derivative_roll); //Scaled by .01 to bring within -1 to 1 range
-
-  //Pitch PID
-  float error_pitch = pitch_des - ahr.pitch;
-  integral_pitch += error_pitch * imu.dt;
-  integral_pitch = constrain(integral_pitch, -i_limit, i_limit); //Saturate integrator to prevent unsafe buildup
-  float derivative_pitch = ahr.gy; 
-  pid.pitch = 0.01 * (Kp_ro_pi_angle*error_pitch + Ki_ro_pi_angle*integral_pitch - Kd_ro_pi_angle*derivative_pitch); //Scaled by .01 to bring within -1 to 1 range
-
-  //Yaw PID
-  if(-0.02 < wifi_rcl.yaw && wifi_rcl.yaw < 0.02) {
-    //on reset, set desired yaw to current yaw
-    if(zero_integrators) yaw_desired = ahr.yaw; 
-
-    //Yaw stick centered: hold yaw_desired
-    float error_yaw = degreeModulus(yaw_desired - ahr.yaw);
-    float desired_yawRate = error_yaw / 0.5; //set desired yawRate such that it gets us to desired yaw in 0.5 second
-    float derivative_yaw = desired_yawRate - ahr.gz;
-    pid.yaw = 0.01 * (Kp_yaw_angle*error_yaw + Kd_yaw_angle*derivative_yaw); //Scaled by .01 to bring within -1 to 1 range
-
-    //update yaw rate controller
-    error_yawRate_prev = 0;
-  }else{
-    //Yaw stick not centered: stablize on rate from GyroZ
-    float error_yawRate = yawRate_des - ahr.gz;
-    integral_yawRate += error_yawRate * imu.dt;
-    integral_yawRate = constrain(integral_yawRate, -i_limit, i_limit); //Saturate integrator to prevent unsafe buildup
-    float derivative_yawRate = (error_yawRate - error_yawRate_prev) / imu.dt; 
-    pid.yaw = 0.01 * (Kp_yaw_rate*error_yawRate + Ki_yaw_rate*integral_yawRate + Kd_yaw_rate*derivative_yawRate); //Scaled by .01 to bring within -1 to 1 range
-
-    //Update derivative variables
-    error_yawRate_prev = error_yawRate;
-
-    //update yaw controller: 
-    yaw_desired = ahr.yaw; //set desired yaw to current yaw, the yaw angle controller will hold this value
-  }
-}
-
-void out_KillSwitchAndFailsafe() {
-  //Change to ARMED when wifi_rcl is armed (by switch or stick command)
-  if (!out.armed && wifi_rcl.armed) {
-    out.armed = true;
-    Serial.println("OUT: ARMED");
-  }
-
-  //Change to DISARMED when wifi_rcl is disarmed, or if radio lost connection
-  if (out.armed && (!wifi_rcl.armed || !wifi_rcl.connected())) {
-    out.armed = false;
-    if(!wifi_rcl.armed) {
-      Serial.println("OUT: DISARMED");
-    }else{
-      Serial.println("OUT: DISARMED due to lost radio connection");
-    }
-  }
-}
-
-void out_Mixer() {
-  //DESCRIPTION: Mixes scaled commands from PID controller to actuator outputs based on vehicle configuration
-  /*
-   * Takes pid.roll, pid.pitch, and pid.yaw computed from the PID controller and appropriately mixes them for the desired
-   * vehicle configuration. For example on a quadcopter, the left two motors should have +pid.roll while the right two motors
-   * should have -pid.roll. Front two should have +pid.pitch and the back two should have -pid.pitch etc... every motor has
-   * normalized (0 to 1) wifi_rcl.throttle command for throttle control. Can also apply direct unstabilized commands from the transmitter with 
-   * wifi_rcl.xxx variables are to be sent to the motor ESCs and servos.
-   * 
-   *Relevant variables:
-   *wifi_rcl.throtle - direct thottle control
-   *pid.roll, pid.pitch, pid.yaw - stabilized axis variables
-   *wifi_rcl.roll, wifi_rcl.pitch, wifi_rcl.yaw - direct unstabilized command passthrough
-   */
-/*
-Motor order diagram (Betaflight order)
-
-      front
- CW -->   <-- CCW
-     4     2 
-      \ ^ /
-       |X|
-      / - \
-     3     1 
-CCW -->   <-- CW
-
-                                        M1234
-Pitch up (stick back)   (front+ back-)   -+-+
-Roll right              (left+ right-)   --++
-Yaw right               (CCW+ CW-)       -++-
-*/
-
-  // IMPORTANT: This is a safety feature to remind the pilot to disarm.
-  // Set motor outputs to at least armed_min_throttle, to keep at least one prop spinning when armed. The [out] module will disable motors when out.armed == false
-  float thr = armed_min_throttle + (1 - armed_min_throttle) * wifi_rcl.throttle; //shift motor throttle range from [0.0 .. 1.0] to [armed_min_throttle .. 1.0]
-
-  if(wifi_rcl.throttle == 0) {
-    //if throttle idle, then run props at low speed without applying PID. This allows for stick commands for arm/disarm.
-    out.set(0, thr);
-    out.set(1, thr);
-    out.set(2, thr);
-    out.set(3, thr);
-  }else{
-    // Quad mixing
-    out.set(0, thr - pid.pitch - pid.roll - pid.yaw); //M1 Back Right CW
-    out.set(1, thr + pid.pitch - pid.roll + pid.yaw); //M2 Front Right CCW
-    out.set(2, thr - pid.pitch + pid.roll + pid.yaw); //M3 Back Left CCW
-    out.set(3, thr + pid.pitch + pid.roll - pid.yaw); //M4 Front Left CW
-  }
+  Serial.println();
 }
